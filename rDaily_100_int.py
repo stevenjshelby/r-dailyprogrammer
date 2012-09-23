5d6747eff574d9a06db5f3772c305a627a1bff52
Steven <theUberPwnzor@gmail.com>
2012-09-23 18:25:12 -0400

    added int 100

---
+++ rDaily_100_int.py
@@ -0,0 +1,24 @@
+#Python Filler Text Generator
+#r/dailyprogrammer
+#100 intermediate
+
+import sys, re
+
+#define some statics
+CHARS_TO_KEEP = 1
+MATCH_PARTIALS = True
+
+#the data
+uncensored = sys.stdin.read()
+censored = uncensored
+filter = [dword.strip() for dword in open('swearWords.txt','r')]
+
+for dword in filter:
+	if not MATCH_PARTIALS:
+		dword = ' ' + dword + ' '
+	
+	if re.search(dword,censored,flags=re.I):
+		censored = re.sub(dword[CHARS_TO_KEEP:],'*' * (len(dword)-CHARS_TO_KEEP),censored,flags=re.I)
+
+#output
+print(censored)
